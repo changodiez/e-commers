69c70bbb675577dc1d0b9dc9e072562d0e6186ba
RicardoJDG <ricardojdg13@gmail.com>
2020-08-11 12:45:57 +0200

    Added a README with the packages installed. Divided the project in 2 folders for frontend and backend, and added some starting code for the backend, although we won't use it yet.

---
+++ server/db.js
@@ -0,0 +1,5 @@
+const { Pool } = require("pg");
+
+const pool = "TBD"; //Once we create the database we will put the info for the pool connection here
+
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const port = 4000;
+
+//middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTES
+/*TODO*/
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
