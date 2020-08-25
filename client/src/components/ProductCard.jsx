742dccb249614a74c71eed2c7382ea164ee97cbe
RicardoJDG <ricardojdg13@gmail.com>
2020-08-25 14:18:42 +0200

    The Grid is working although a bit ugly

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const ProductCard = () => {
-    return ( 
-        <div>
-            
-        </div>
-     );
-}
- 
+const ProductCard = (props) => {
+  return (
+    <div
+      className="card text-center p-3 overflow-auto h-50 mh-50"
+      style={{ overflowY: "scroll" }}
+    >
+      <img className="card-img-top" src={props.value.image} alt="Product" />
+      <div className="card-body ">
+        <h5 className="card-title">{props.value.title}</h5>
+        <p className="card-text">{props.value.description}</p>
+        <button onClick={console.log("To define")} className="btn btn-primary">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default ProductCard;
