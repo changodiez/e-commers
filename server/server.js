209eef2923c7135c081ccb0f2cf3b58c87254221
RicardoJDG <ricardojdg13@gmail.com>
2020-08-28 11:08:58 +0200

    Created routes skeleton and middleware for info validation

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //ROUTES
+
+//* Product Route *//
+app.use("/products", require("./routes/products"));
+
 /*TODO*/
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
