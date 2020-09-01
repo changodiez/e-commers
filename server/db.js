eec8215a730d25ee00ba70755604d1fb6765b905
changodiez <joaquindiezw@gmail.com>
2020-09-01 23:28:56 +0200

    server fixed

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "migracode",
+  user: "postgres",
   host: "localhost",
   database: "etailor",
   password: "password",
   port: 5432,
 });
 
+module.exports = pool;
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,18 @@
-const router = require("express").Router();
 const pool = require("../db");
+const router = require("express").Router();
 
-router.get("/", async (req, res) => {});
+
+router.get("/", async (req, res) => {
+  const dbquery = "SELECT * FROM products";
+
+  try {
+    const allProducts = await pool.query(dbquery);
+    res.json(allProducts.rows);
+  } catch (error) {
+      console.error (error.message)
+      res.status(error.status).json("Something went wrong")
+  }
+});
+
 
 module.exports = router;
